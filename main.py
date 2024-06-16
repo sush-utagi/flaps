a5d6a5a47d18e42c98885cb44d80bdddb1bd66d6
sush-utagi <127123817+sush-utagi@users.noreply.github.com>
2024-06-16 15:34:04 +0800

    assets loaded

--- main.py
+++ main.py
@@ -1,1 +1,55 @@
-print("hello\n")
+import os
+import time
+import random
+import pygame
+import neat
+
+# Set the window size
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 800
+
+# Load the images
+BIRD_IMAGES = [pygame.transform.scale2x(pygame.image.load(os.path.join("images", "bird1.png"))),
+               pygame.transform.scale2x(pygame.image.load(os.path.join("images", "bird2.png"))),
+               pygame.transform.scale2x(pygame.image.load(os.path.join("images", "bird3.png")))]
+
+PIPE_IMAGE = pygame.transform.scale2x(pygame.image.load(os.path.join("images", "pipe.png")))
+BASE_IMAGE = pygame.transform.scale2x(pygame.image.load(os.path.join("images", "base.png")))
+BACKGROUND_IMAGE = pygame.transform.scale2x(pygame.image.load(os.path.join("images", "background.png")))
+
+
+class Bird:
+    IMAGES = BIRD_IMAGES
+    MAX_ROTATION = 25
+    ROTATION_VELOCITY = 20
+    ANIMATION_TIME = 5
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.tick_count = 0
+        self.velocity = 0
+        self.height = self.y
+        self.image_count = 0
+        self.image = self.IMAGES[0]
+
+    def jump(self):
+        self.velocity   = -10.5       #
+        self.tick_count =  0
+        self.height     =  self.y
+
+    def move(self):
+        print(f"bird moving at: {self.velocty}(m/s)")
+
+        self.tick_count += 1
+
+        displacement = self.velocity*self.tick_count + 1.5*self.tick_count**2
+
+        if displacement >= 16:
+            displacement = 16
+
+        if displacement < 0:
+            displacement -= 2
+
+        
